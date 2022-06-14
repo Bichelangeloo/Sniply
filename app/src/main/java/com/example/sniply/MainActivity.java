d7d9a05c964e1f28281ae3c0519867970a0c5942
LimeT97 <lesko@spse-po.sk>
2022-06-14 14:35:52 +0200

    deleted timer button

--- app/src/main/java/com/example/sniply/MainActivity.java
+++ app/src/main/java/com/example/sniply/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.sniply;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
